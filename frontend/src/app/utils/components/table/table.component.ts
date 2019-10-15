832bca577c1b9e4f773c451d1f61bcee02708464
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-15 00:07:26 -0500

    Generic data table

---
+++ frontend/src/app/utils/components/table/table.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+
+interface Action {
+  name: string[];
+  route: string[];
+  parameters: { [prop: string]: string }[];
+  icon: string;
+  color: string;
+  idPropertyName: string;
+}
+
+interface Column {
+  title: string;
+  property: string;
+}
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+})
+export class TableComponent implements OnInit {
+  @Input() getElementsEndpoint: string;
+  @Input() deleteElementEndpoint: string;
+  @Input() actions: Action[];
+  @Input() columns: Column[];
+  @Input() pageSize = 10;
+  tableColumns: string[] = [];
+  loading = true;
+  data: any[] = [];
+  constructor(private router: Router, private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.tableColumns = this.columns.map((column: Column) => column.title);
+    this.getElements();
+  }
+
+  getElements() {
+    this.loading = true;
+    this.apiService.get(this.getElementsEndpoint, {}).subscribe(
+      (elements: any[]) => {
+        this.data = elements;
+      },
+      () => {},
+      () => {
+        this.loading = false;
+      },
+    );
+  }
+
+  triggerAction(object: any, action: Action) {
+    const params = {
+      ...action.parameters,
+    };
+    params[action.idPropertyName] = object.id;
+    this.router.navigate(action.route, {
+      queryParams: params,
+    });
+  }
+}
--- frontend/src/app/utils/utils.module.ts
+++ frontend/src/app/utils/utils.module.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ApiService } from './services/api.service';
 import { StorageService } from './services/storage.service';
 import { MessagesService } from './services/messages.service';
-
-
+import { TableComponent } from './components/table/table.component';
+import {
+  MatTableModule,
+  MatButtonModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+} from '@angular/material';
 
 @NgModule({
-  declarations: [],
+  declarations: [TableComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
   ],
-  providers: [ApiService, StorageService, MessagesService]
+  providers: [ApiService, StorageService, MessagesService],
 })
-export class UtilsModule { }
+export class UtilsModule {}
