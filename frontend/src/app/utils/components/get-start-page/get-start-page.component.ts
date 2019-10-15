74546452e19bbb2f7290df7c26c205265521f58d
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-15 01:02:44 -0500

    Redirect to start page

--- frontend/src/app/utils/components/get-start-page/get-start-page.component.ts
+++ frontend/src/app/utils/components/get-start-page/get-start-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../../services/storage.service';
+import { ApiService } from '../../services/api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-get-start-page',
   templateUrl: './get-start-page.component.html',
-  styleUrls: ['./get-start-page.component.scss']
+  styleUrls: ['./get-start-page.component.scss'],
 })
 export class GetStartPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router) {}
 
   ngOnInit() {
+    this.apiService.me().subscribe((me: any) => {
+      if (me.name === 'superuser') {
+        this.router.navigateByUrl('/superuser');
+      } else {
+        this.router.navigateByUrl('/admin');
+      }
+    });
   }
-
 }
