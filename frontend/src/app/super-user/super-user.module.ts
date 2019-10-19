fc5266184d7c82c6bf9224a9edb13051b96367db
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-19 15:52:25 -0500

    Moved component to the right directory

--- frontend/src/app/super-user/super-user.module.ts
+++ frontend/src/app/super-user/super-user.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SuperuserStartPageComponent } from './components/superuser-start-page/superuser-start-page.component';
 import { UtilsModule } from '../utils/utils.module';
+import { AdminsComponent } from './components/admins/admins.component';
 
-const components = [SuperuserStartPageComponent];
+const components = [SuperuserStartPageComponent, AdminsComponent];
 
 @NgModule({
   declarations: components,
