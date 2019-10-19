7f4bc2464c036da95ecbc0edfb4ac1694d2829c9
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-19 17:24:33 -0500

    Use table component

--- frontend/src/app/super-user/components/admins/admins.component.ts
+++ frontend/src/app/super-user/components/admins/admins.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Action, Column } from 'src/app/utils/models/table';
+import { QueryParameters } from 'src/app/utils/models/url';
 
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
-  styleUrls: ['./admins.component.scss']
+  styleUrls: ['./admins.component.scss'],
 })
 export class AdminsComponent implements OnInit {
+  actions: Action[] = [
+    {
+      name: 'edit',
+      route: ['/', 'new-admin'],
+      parameters: {},
+      icon: '',
+      color: 'black',
+      idPropertyName: 'id',
+    },
+    {
+      name: 'delete',
+      route: [],
+      parameters: {},
+      icon: '',
+      color: 'black',
+      idPropertyName: 'id',
+    },
+  ];
+  columns: Column[] = [
+    {
+      title: 'Name',
+      property: 'name',
+    },
+  ];
+  newElementRoute = ['/', 'new-admin'];
 
-  constructor() { }
+  getElementsEndpoint = '/users/jsonata';
+  getElementsQueryParams: QueryParameters = {
+    query: `(
+      [$]
+    )`,
+  };
 
-  ngOnInit() {
-  }
+  deleteElementEndpoint = '/users';
 
+  constructor() {}
+
+  ngOnInit() {}
 }
