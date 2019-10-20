78b91298843c3ff2b00d5f22c8dfc9f49b827287
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-19 21:25:08 -0500

    File rename

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -1,4 +1,4 @@
 export * from './user.controller';
-export * from './elemento.controller';
+export * from './element.controller';
 export * from './global-element.controller';
 export * from './datum.controller';
