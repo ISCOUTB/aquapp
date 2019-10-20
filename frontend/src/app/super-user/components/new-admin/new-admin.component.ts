b7116447cf95fb11ea995c278081b6bd11666a5d
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-19 19:13:53 -0500

    Create new admin users

--- frontend/src/app/super-user/components/new-admin/new-admin.component.ts
+++ frontend/src/app/super-user/components/new-admin/new-admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/utils/services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-new-admin',
@@ -7,7 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewAdminComponent implements OnInit {
   title = 'Nuevo usuario administrador';
-  constructor() {}
+  form: FormGroup;
+  id: string;
+  loading = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute,
+    public location: Location,
+  ) {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.id = this.activatedRoute.snapshot.queryParams.id;
+  }
+
+  newAdmin() {
+    this.loading = true;
+    this.apiService.post('/users', {}, this.form.value).subscribe({
+      next: () => {
+        this.location.back();
+      },
+      error: () => {},
+      complete: () => (this.loading = false),
+    });
+  }
+
+  editAdmin() {}
 }
--- frontend/src/app/super-user/super-user.module.ts
+++ frontend/src/app/super-user/super-user.module.ts
@@ -4,6 +4,12 @@ import { SuperuserStartPageComponent } from './components/superuser-start-page/s
 import { UtilsModule } from '../utils/utils.module';
 import { AdminsComponent } from './components/admins/admins.component';
 import { NewAdminComponent } from './components/new-admin/new-admin.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule,
+} from '@angular/material';
 
 const components = [
   SuperuserStartPageComponent,
@@ -13,7 +19,15 @@ const components = [
 
 @NgModule({
   declarations: components,
-  imports: [CommonModule, UtilsModule],
+  imports: [
+    CommonModule,
+    UtilsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: components,
 })
 export class SuperUserModule {}
