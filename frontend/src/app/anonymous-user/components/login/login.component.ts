f82da5b1b5f33e48838b515ceb34ddaf7a845c13
Juan Pablo Rodríguez Macias <esperanto@pop-os.localdomain>
2019-10-24 15:27:14 -0500

    Use overlay to position the login

--- frontend/src/app/anonymous-user/components/login/login.component.ts
+++ frontend/src/app/anonymous-user/components/login/login.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewContainerRef,
+  ViewChild,
+  AfterViewInit,
+  TemplateRef,
+} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/utils/services/api.service';
 import { debounceTime } from 'rxjs/operators';
 import { MyErrorStateMatcher } from 'src/app/utils/services/form-tools.service';
 import { MessagesService } from 'src/app/utils/services/messages.service';
 import { MESSAGES } from 'src/app/messages';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements AfterViewInit {
   form: FormGroup;
   incorrectCredentials = false;
   errorStateMatcher = new MyErrorStateMatcher();
+  overlayRef: OverlayRef;
+  @ViewChild('templatePortalContent', { static: false })
+  templatePortalContent: TemplateRef<any>;
   constructor(
     private formBuilder: FormBuilder,
     private apiService: ApiService,
     private messageService: MessagesService,
+    private overlay: Overlay,
+    private viewContainerRef: ViewContainerRef,
   ) {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
@@ -30,7 +44,21 @@ export class LoginComponent implements OnInit {
     this.messageService.sendMessage({ name: MESSAGES.hideToolbar, value: '' });
   }
 
-  ngOnInit() {}
+  ngAfterViewInit() {
+    this.overlayRef = this.overlay.create({
+      width: 300,
+      height: 320,
+      disposeOnNavigation: true,
+      positionStrategy: this.overlay
+        .position()
+        .global()
+        .centerVertically()
+        .centerHorizontally(),
+    });
+    this.overlayRef.attach(
+      new TemplatePortal(this.templatePortalContent, this.viewContainerRef),
+    );
+  }
 
   login() {
     this.apiService
