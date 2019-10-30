9acaa4328ff16f229485f73799ec207bb63bc436
Jairo Serrano <jairo.serrano@gmail.com>
2019-10-30 12:37:13 -0500

    problemas de conectividad interna no solucionados

--- api/config.local.js
+++ api/config.local.js
@@ -2,6 +2,6 @@ var p = require('../package.json');
 var version = p.version.split('.').shift();
 module.exports = {
     restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || 'api',
     port: process.env.PORT || 3000
 };
