d52ee4be50f752de9ca294d3c5a0bdf14049d41f
Jairo Serrano <jairo.serrano@gmail.com>
2019-10-30 11:54:59 -0500

    Integración de la API, redirecciones

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'http://aquapp.utb.edu.co/api/v1',
   mapUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 };
