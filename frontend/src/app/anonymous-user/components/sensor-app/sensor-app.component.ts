ac38adac5cf71a56e91eb738bfd34bfd8eada966
EsperantoDeparture <esperanto@pop-os.localdomain>
2019-11-06 17:18:26 -0500

    Show empty map

--- frontend/src/app/anonymous-user/components/sensor-app/sensor-app.component.ts
+++ frontend/src/app/anonymous-user/components/sensor-app/sensor-app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MessagesService } from 'src/app/utils/services/messages.service';
 import { MESSAGES } from 'src/app/messages';
+import { tileLayer, latLng, LatLngBounds, Map } from 'leaflet';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-sensor-app',
@@ -8,6 +10,25 @@ import { MESSAGES } from 'src/app/messages';
   styleUrls: ['./sensor-app.component.scss'],
 })
 export class SensorAppComponent implements OnInit {
+  mapStyle: any = {
+    height: `${window.innerHeight - 64}px`,
+    width: '100%',
+  };
+  mapOptions = {
+    layers: [
+      tileLayer(environment.mapUrl, {
+        maxZoom: 18,
+        attribution: 'Map data © OpenStreetMap contributors',
+      }),
+    ],
+    zoom: 13.5,
+    center: latLng(10.4241961, -75.535),
+  };
+  mapBounds = new LatLngBounds(
+    latLng(10.371076, -75.466699),
+    latLng(10.369281, -75.463776),
+  );
+  map: Map;
   constructor(private messageService: MessagesService) {}
 
   ngOnInit() {
@@ -16,5 +37,31 @@ export class SensorAppComponent implements OnInit {
       name: MESSAGES.fullWidthContent,
       value: {},
     });
+  }
+
+  onMapReady(map: Map) {
+    this.map = map;
+    window.addEventListener('resize', () => this.fixMap());
+    setTimeout(() => {
+      this.fixMap();
+      /*this.messageService.sendMessage({
+        name: MESSAGES.showSplashScreen,
+        value: {},
+      });*/
+    }, 300);
+  }
+
+  fixMap() {
+    this.mapStyle =
+      this.mapStyle === undefined
+        ? {
+            height: `${window.innerHeight - 64}px`,
+            width: '100%',
+          }
+        : this.mapStyle;
+    this.map.invalidateSize();
+    if (this.mapBounds) {
+      this.map.fitBounds(this.mapBounds);
+    }
   }
 }
