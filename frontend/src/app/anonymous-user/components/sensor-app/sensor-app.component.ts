1b581172d44580a34e28e94935ad0f4ee85f8066
EsperantoDeparture <esperanto@pop-os.localdomain>
2019-11-06 18:11:57 -0500

    Show points as cluster

--- frontend/src/app/anonymous-user/components/sensor-app/sensor-app.component.ts
+++ frontend/src/app/anonymous-user/components/sensor-app/sensor-app.component.ts
@@ -3,6 +3,13 @@ import { MessagesService } from 'src/app/utils/services/messages.service';
 import { MESSAGES } from 'src/app/messages';
 import { tileLayer, latLng, LatLngBounds, Map } from 'leaflet';
 import { environment } from 'src/environments/environment';
+import { ApiService } from 'src/app/utils/services/api.service';
+import { JSONataResponse } from 'src/app/utils/models/url';
+import { MarkerClusterLayer, Layer } from 'src/app/utils/models/layer';
+import { MapService } from 'src/app/utils/services/map.service';
+declare let L;
+import 'leaflet';
+import 'leaflet.markercluster';
 
 @Component({
   selector: 'app-sensor-app',
@@ -29,7 +36,14 @@ export class SensorAppComponent implements OnInit {
     latLng(10.369281, -75.463776),
   );
   map: Map;
-  constructor(private messageService: MessagesService) {}
+  form = '5dc341823153fa33d0225b11';
+  routes: any[];
+  layers: Layer[] = [];
+  constructor(
+    private messageService: MessagesService,
+    private apiService: ApiService,
+    private mapService: MapService,
+  ) {}
 
   ngOnInit() {
     this.messageService.sendMessage({ name: MESSAGES.closeSidenav, value: {} });
@@ -39,10 +53,60 @@ export class SensorAppComponent implements OnInit {
     });
   }
 
+  async init() {
+    await this.apiService
+      .get('/elements/open/jsonata', {
+        query: `([$[form="${this.form}"]])`,
+      })
+      .toPromise()
+      .then((routes: JSONataResponse) => {
+        this.routes = routes.data;
+      });
+    for (const route of this.routes) {
+      route.data = await this.apiService
+        .get('/data/open/vm2', {
+          query: `this.data`,
+          additionalFilters: JSON.stringify([{ trackedObject: route.id }]),
+        })
+        .toPromise();
+    }
+    this.setupLayers();
+  }
+
+  setupLayers() {
+    console.log(this.layers);
+    for (const route of this.routes) {
+      console.log((route.data || []).map(d => [d.latitude, d.longitude]));
+      this.layers.push(
+        new MarkerClusterLayer(
+          'Posiciones',
+          '',
+          {},
+          true,
+          false,
+          (route.data || []).map(d => [d.latitude, d.longitude]),
+        ),
+      );
+    }
+    console.log(this.layers);
+    this.updateLayers();
+  }
+
+  updateLayers() {
+    for (const layer of this.layers) {
+      if (layer.active && !layer.loaded) {
+        this.mapService.addLayer(layer, this.map);
+      } else if (!layer.active && layer.loaded) {
+        this.mapService.removeLayer(layer, this.map);
+      }
+    }
+  }
+
   onMapReady(map: Map) {
     this.map = map;
     window.addEventListener('resize', () => this.fixMap());
     setTimeout(() => {
+      this.init();
       this.fixMap();
       /*this.messageService.sendMessage({
         name: MESSAGES.showSplashScreen,
--- frontend/src/app/utils/services/map.service.ts
+++ frontend/src/app/utils/services/map.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@angular/core';
-import { MarkerLayer, GeoJSONLayer, Layer } from '../models/layer';
+import {
+  MarkerLayer,
+  GeoJSONLayer,
+  Layer,
+  MarkerClusterLayer,
+} from '../models/layer';
 import { Map } from 'leaflet';
+import 'leaflet';
+import 'leaflet.markercluster';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +22,8 @@ export class MapService {
       }
     } else if (layer instanceof GeoJSONLayer) {
       layer.figures.addTo(map);
+    } else if (layer instanceof MarkerClusterLayer) {
+      map.addLayer(layer.cluster);
     }
     layer.active = true;
     layer.loaded = true;
@@ -27,6 +36,8 @@ export class MapService {
       }
     } else if (layer instanceof GeoJSONLayer) {
       layer.figures.removeFrom(map);
+    } else if (layer instanceof MarkerClusterLayer) {
+      layer.cluster.removeFrom(map);
     }
     layer.active = false;
     layer.loaded = false;
